9ca8c2e951baa98d78cfe0646f65683f268bbbf2
Paul Morelle <paul.morelle@gmail.com>
2011-07-25 19:50:18 +0200

    Brand-new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kabona::Application.config.secret_token = 'bdd92395b0553f692af2c64069a74cc12c090c051961218c46c403658deef3ce656ec6864b60572e6ea15960d3f458ae0e6a49fdec9d22b612a12e9988824d73'
