e40e1e39575b63d71cf1502773d6a5cb59708830
Paul Morelle <paul.morelle@gmail.com>
2011-08-21 13:06:05 +0200

    Upgraded rails from 3.0.4 to 3.1.0.rc6

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Kabona::Application.config.session_store :cookie_store, :key => '_kabona_session'
+Kabona::Application.config.session_store :cookie_store, key: '_kabona_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
